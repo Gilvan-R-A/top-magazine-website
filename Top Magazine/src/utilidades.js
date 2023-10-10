fece32ae7b469398ac47ce210dd9a0e18b36b18a
gilvan alves <gilvan.dev@gmail.com>
2023-10-10 14:15:56 -0300

    adicionando página de histórico de pedidos

--- Top Magazine/src/utilidades.js
+++ Top Magazine/src/utilidades.js
@@ -139,7 +139,7 @@ export function desenharProdutoNoCarrinhoSimples(idProduto, idContainerHTML, qua
   const containerProdutosCarrinho = document.getElementById(idContainerHTML);
 
   const elementoArticle = document.createElement("article");
-  const articleClasses = ["flex", "bg-stone-200", "rounded-lg", "p-1", "relative", "mb-2"];
+  const articleClasses = ["flex", "bg-stone-200", "rounded-lg", "p-1", "relative", "mb-2", "w-96"];
   for (const articleClass of articleClasses) {
     elementoArticle.classList.add(articleClass);
   }
