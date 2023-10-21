56eb95693de8c0a5939880a8197ba52149f33b1d
gilvan alves <gilvan.dev@gmail.com>
2023-10-21 09:58:20 -0300

    atualizando vite.config

--- Top Magazine/vite.config.js
+++ Top Magazine/vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/Top_Magazine/",
+  base: "/top-magazine/",
   plugins: [],
   resolve: {
     /*something*/
