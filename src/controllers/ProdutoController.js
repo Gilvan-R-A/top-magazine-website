fde51d9d3ed717aa690f45d2f037411943c2b02d
Gilvan-R-A <gilvan.dev@gmail.com>
2025-06-19 16:26:40 -0300

    fix: fixes product display when added to shopping cart

--- src/controllers/ProdutoController.js
+++ src/controllers/ProdutoController.js
@@ -1,21 +1,42 @@
 import { ProdutoModel } from "@models/ProdutoModel.js"
 import { CarrinhoModel } from "@models/CarrinhoModel.js";
+import { CarrinhoView } from "@views/CarrinhoView.js";
 
 export class ProdutoController {
     constructor(produtoView) {
         this.view = produtoView;
         this.view.controller = this;
         this.carrinho = new CarrinhoModel();
+        this.catalogo = ProdutoModel.listar();
+        this.carrinhoView = new CarrinhoView(this.carrinho, this.catalogo);
     }
 
     inicializar() {
-        const catalogo = ProdutoModel.listar();
-        this.view.renderizarCatalogo(catalogo);
+        this.view.renderizarCatalogo(this.catalogo);
         this.view.configurarBotoesFiltro();
+        this.atualizarCarrinhoView();
     }
 
     adicionarAoCarrinho(idProduto) {
         this.carrinho.adicionar(idProduto);
+        this.atualizarCarrinhoView();
+    }
+
+    atualizarCarrinhoView() {
+        this.carrinhoView.renderizarTodos({
+            remover: (id) => {
+                this.carrinho.remover(id);
+                this.atualizarCarrinhoView();
+            },
+            incrementar: (id) => {
+                this.carrinho.incrementar(id);
+                this.atualizarCarrinhoView();
+            },
+            decrementar: (id) => {
+                this.carrinho.decrementar(id);
+                this.atualizarCarrinhoView();
+            }
+        });
     }
 
     filtrar(tipo) {
